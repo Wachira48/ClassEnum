6cf2d3ed76a655f0b04dd7a85e8420ccb621a821
mario <mariosakkamoto@gmail.com>
2021-03-08 21:15:14 -0300

    AprendendoEnum

---
+++ src/main/kotlin/Enum.kt
@@ -0,0 +1,12 @@
+enum class Prioridade1 {
+    BAIXA, MEDIA, ALTA
+}
+enum class Prioridade2(val id: Int) {
+    BAIXA (1), MEDIA(5), ALTA(10)
+}
+
+fun main() {
+    for (p in Prioridade2.values()) {
+        println("$p - ${p.id}")
+    }
+}
